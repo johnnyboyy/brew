15d801dfa7bbf1e97ede42302a027de572fe58cc
Johnnyboyy <zdanis.john@gmail.com>
2015-11-29 10:27:12 -0800

    added a custom editable stylesheet

--- library/bones.php
+++ library/bones.php
@@ -135,6 +135,9 @@ function bones_scripts_and_styles() {
     // register main stylesheet
     wp_register_style( 'bones-stylesheet', get_template_directory_uri() . '/library/css/style.css', array(), '', 'all' );
 
+ 	// register custom stylesheet
+    wp_register_style( 'bones-custom', get_template_directory_uri() . 'custom.css', array(), '', 'all' );
+
     // ie-only style sheet
     wp_register_style( 'bones-ie-only', get_template_directory_uri() . '/library/css/ie.css', array(), '' );
 
@@ -156,6 +159,7 @@ function bones_scripts_and_styles() {
     wp_enqueue_script( 'bones-modernizr' );
     wp_enqueue_style( 'bones-stylesheet' );
     wp_enqueue_style( 'bones-ie-only' );
+    wp_enqueue_style( 'bones-custom' );
 
     $wp_styles->add_data( 'bones-ie-only', 'conditional', 'lt IE 9' ); // add conditional wrapper around ie stylesheet
 

1879d495b60558020fac1124c2993785b7af275c
Johnnyboyy <zdanis.john@gmail.com>
2016-09-27 10:20:48 -0700

    changed to a basic layout so Visual Composer will work

--- page-home.php
+++ page-home.php
@@ -6,43 +6,21 @@ Template Name: Home Page Template
 
 <?php get_header(); ?>
 
-<div class="homebanner">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-8 offset-md-2">
-        <h1>BREW Updated for Wordpress</h1>
-        <p>A free and open-source starter theme based on BREW. Updated with Bootstrap 4</p>
-        <p><a href="http://www.github.com/johnnyboyy/brew" target="_blank" class="btn btn-default btn-lg">Github</a>
-          <a href="http://johnzdanis.com" target="_blank" class="btn btn-lg btn-primary">Author</a></p>
-      </div>
-    </div><!-- end .row-->
-  </div> <!-- end .container-->
-</div> <!-- end #banner-->
-
 <div class="container">
-  <div class="row text-xs-center">
+  <div id="main" class="row" role="main">
 
-    <!--Section 1-->
-    <div class="col-sm-4 about">
-      <i class="fa fa-laptop fa-4x light-gray"></i>
-      <h3>Built with Bootstrap 4</h3>
-      <p>Updated to the newest bootstrap</p>
-    </div>
+    <?php if (have_posts()) :?>
+      <?php while (have_posts()) : the_post(); ?>
 
-    <!--Section 2-->
-    <div class="col-sm-4 about">
-      <i class="fa fa-github-square fa-4x light-gray"></i>
-      <h3>Free and Open-Source</h3>
-      <p>Credit to <a href="http://danvswild.com" target="_blank" class="">Dan Brown</a> for <a href="http://www.github.com/slightlyoffbeat/brew" target="_blank">BREW</a>
-      which this theme was based on.</p>
-    </div>
+        <div class="col-md-12 clearfix">
+          <h1><?php the_title(); ?></h1>
+          <div class="content">
+            <?php the_content(); ?>
+          </div>
+        </div>
 
-    <!--Section 3-->
-    <div class="col-sm-4 about">
-      <i class="fa fa-flag fa-4x light-gray"></i>
-      <h3>Over 350 Icons</h3>
-      <p>Font Awesome.</p>
-    </div>
+      <?php endwhile; ?>
+    <?php endif;?>
 
   </div>
 </div>
